89e4e5cf1796e68f08bf5117f13b91aa34d318d7
pablobernalmoreno <pbm1998@hotmail.com>
2019-11-05 14:18:09 -0800

    Limpiando

--- BackUp_Plan/Epsilon_Simple.m
+++
@@ -1,8 +0,0 @@
-d = single(0.5);
-i = 0;
-while 1 ~= 1 + d;
-    disp(d);
-    d = d/2;
-    i = i +1;
-end;
-disp(i);
--- BackUp_Plan/g.m
+++
@@ -1,3 +0,0 @@
-function y = g(x)
-y = exp((-x^2)-1) - x * sin(2*x+3) -4*x + 4;
-end
