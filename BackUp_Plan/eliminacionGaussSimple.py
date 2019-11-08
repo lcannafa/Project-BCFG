70d0b4f52c9b127acfba5dda057187f2922477f7
pablobernalmoreno <pbm1998@hotmail.com>
2019-11-08 13:54:13 -0800

    Force the use of floats

--- BackUp_Plan/eliminacionGaussSimple.py
+++ BackUp_Plan/eliminacionGaussSimple.py
@@ -3,18 +3,17 @@ from funciones import *
 import sys
 #matrix = str(raw_input("Please enter the coefficient matrix of the system: "))
 #vector = str(raw_input("Please enter the independent terms of the system: "))
-matrix = '2.0 -3.0 4.0 1.0;-4.0 2.0 1.0 -2.0;1.0 3.0 -5.0 3.0;-3.0 -1.0 1.0 -1.0'
-vector = '10.0 -10.0 32.0 -21.0'
-a = np.matrix(matrix)
-b = np.matrix(vector)
+matrix = '2 -3 4 1;-4 2 1 -2;1 3 -5 3;-3 -1 1 -1'
+vector = '10 -10 32 -21'
+a = np.matrix(matrix).astype(float)
+b = np.matrix(vector).astype(float)
 b = b.T
 s = np.hstack(((a,b)))
-n = len(s) 
+n = len(s)
 print(n)
 for k in range (0,(n-1)):
     for i in range ((k+1),n):
         multiplicador = s[i,k]/s[k,k]
-        print('m ', multiplicador)
         for j in range (k,(n+1)):
             s[i,j] -= multiplicador*s[k,j]
 a = s[:,:-1]
