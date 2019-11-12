4efd548c27b3d0a05d04ba82ccbce8d06286ee6e
pablobernalmoreno <pbm1998@hotmail.com>
2019-11-12 06:56:39 -0800

    Pseudo code for all

---
+++ BackUp_Plan/banda.py
@@ -0,0 +1,13 @@
+import numpy as np
+from funciones import *
+import sys
+
+# a = float vector
+# b = float vector
+# c = float vector
+# d = float vector
+# for k in range(0,(n-1)):
+#   m[k] = a[k]/b[k]
+#   b[k+1] = b[k+1] - m[k]*c[k]
+#   d[k+1] = d[k+1] - m[k]*d[k]
+#return b,d
---
+++ BackUp_Plan/seidel.py
@@ -0,0 +1,14 @@
+import numpy as np
+from funciones import *
+import sys
+
+# tolerancia = float
+# niter = float
+# x = float vector
+# contador = 0
+# dispersion = tolerancia + 1
+# while disperion > tolerancia and contador < niter:
+#   x[1] = calcularNuevoSeidel(x[0])
+#   dispersion = norma(x[1] - x[0])
+#   x[0] = x[1]
+#   contador = contador + 1
