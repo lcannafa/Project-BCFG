ad7ee78a97fc9f2a3285aabe17ba8e2d2c34bdce
pablobernalmoreno <pbm1998@hotmail.com>
2019-11-12 06:30:01 -0800

    Arreglando error de splines

--- BackUp_Plan/splineInterpolation.py
+++ BackUp_Plan/splineInterpolation.py
@@ -8,10 +8,12 @@ strx = '0 1 2 3 4 5'
 x_points = np.fromstring(strx, dtype=float, sep=' ')
 strfx = '12 14 22 39 58 77'
 y_points = np.fromstring(strfx, dtype=float, sep=' ')
+strval = '1.25'
+val = float(strval)
 #x_points = [ 0, 1, 2, 3, 4, 5]
 #y_points = [12,14,22,39,58,77]
 tck = interpolate.splrep(x_points, y_points)
-a = interpolate.splev(1.25, tck)
+a = interpolate.splev(val, tck)
 print(a)
 cs = CubicSpline(x_points,y_points,bc_type='natural')
-print(cs(1.25))
+print(cs(val))
